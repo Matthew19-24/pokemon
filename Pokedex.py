cb6f126efdf6ade7ee40830d9ac5d90df554019d
Matthew19-24 <94659375+Matthew19-24@users.noreply.github.com>
2021-12-25 15:16:16 -0500

    Create Pokedex

---
+++ Pokedex.py
@@ -0,0 +1,62 @@
+import Stats
+import math
+
+
+class Bulbasaur(Stats.Pokemon):
+    def __init__(self, level, owner):
+        super().__init__(level)
+        self.set_iv()
+        self.set_ev()
+        self.set_nature()
+        self.owner = owner
+        self.exp = math.floor(
+            (1.2 * (self.lvl ** 3)) - (15 * (self.lvl ** 2)) + (100 * self.lvl) - 140)
+        self.growth_group = "med_slow"
+        self.type = "Grass"
+        self.BASE_HP = 45
+        self.BASE_ATK = 49
+        self.BASE_DEF = 49
+        self.BASE_SP_ATK = 65
+        self.BASE_SP_DEF = 65
+        self.BASE_SPD = 45
+        self.set_stats()
+
+
+class Charmander(Stats.Pokemon):
+    def __init__(self, level, owner):
+        super().__init__(level)
+        self.set_iv()
+        self.set_ev()
+        self.set_nature()
+        self.owner = owner
+        self.exp = math.floor(
+            (1.2 * (self.lvl ** 3)) - (15 * (self.lvl ** 2)) + (100 * self.lvl) - 140)
+        self.growth_group = "med_slow"
+        self.type = "Fire"
+        self.BASE_HP = 39
+        self.BASE_ATK = 52
+        self.BASE_DEF = 43
+        self.BASE_SP_ATK = 60
+        self.BASE_SP_DEF = 50
+        self.BASE_SPD = 65
+        self.set_stats()
+
+
+class Squirtle(Stats.Pokemon):
+    def __init__(self, level, owner):
+        super().__init__(level)
+        self.set_iv()
+        self.set_ev()
+        self.set_nature()
+        self.owner = owner
+        self.exp = math.floor(
+            (1.2 * (self.lvl ** 3)) - (15 * (self.lvl ** 2)) + (100 * self.lvl) - 140)
+        self.growth_group = "med_slow"
+        self.type = "Water"
+        self.BASE_HP = 44
+        self.BASE_ATK = 48
+        self.BASE_DEF = 65
+        self.BASE_SP_ATK = 50
+        self.BASE_SP_DEF = 64
+        self.BASE_SPD = 43
+        self.set_stats()
