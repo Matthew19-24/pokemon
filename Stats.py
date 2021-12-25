4e5491043570489b52de10ef3c398588ac4c5639
Matthew19-24 <94659375+Matthew19-24@users.noreply.github.com>
2021-12-25 15:15:44 -0500

    Removed individual Pokemon and added to own file

--- Stats.py
+++ Stats.py
@@ -167,7 +167,8 @@ class Pokemon:
 
         elif self.growth_group == "med_slow":
             for n in range(99):
-                if self.exp >= math.floor((1.2 * ((self.lvl + 1) ** 3)) - (15 * ((self.lvl + 1) ** 2)) + (100 * (self.lvl + 1)) - 140):
+                if self.exp >= math.floor(
+                        (1.2 * ((self.lvl + 1) ** 3)) - (15 * ((self.lvl + 1) ** 2)) + (100 * (self.lvl + 1)) - 140):
                     self.lvl += 1
                     print("level up! lvl ", self.lvl)
 
@@ -202,6 +203,7 @@ class Pokemon:
         print(f"\nSpecies: {type(self).__name__}\n" +
               f"Owner: {self.owner}\n" +
               f"Level: {self.lvl}\n" +
+              f"Experience: {self.exp}\n" +
               f"Type: {self.type}\n" +
               f"Nature: {self.nature}\n" +
               f"HP: {self.hp_stat}\n" +
@@ -210,57 +212,3 @@ class Pokemon:
               f"Speed: {self.spd_stat}\n" +
               f"Sp. Attack: {self.sp_atk_stat}\n" +
               f"Sp. Defense: {self.sp_def_stat}")
-
-
-class Bulbasaur(Pokemon):
-    def __init__(self, level, owner):
-        super().__init__(level)
-        self.set_iv()
-        self.set_ev()
-        self.set_nature()
-        self.owner = owner
-        self.growth_group = "med_slow"
-        self.type = "Grass"
-        self.BASE_HP = 45
-        self.BASE_ATK = 49
-        self.BASE_DEF = 49
-        self.BASE_SP_ATK = 65
-        self.BASE_SP_DEF = 65
-        self.BASE_SPD = 45
-        self.set_stats()
-
-
-class Charmander(Pokemon):
-    def __init__(self, level, owner):
-        super().__init__(level)
-        self.set_iv()
-        self.set_ev()
-        self.set_nature()
-        self.owner = owner
-        self.growth_group = "med_slow"
-        self.type = "Fire"
-        self.BASE_HP = 39
-        self.BASE_ATK = 52
-        self.BASE_DEF = 43
-        self.BASE_SP_ATK = 60
-        self.BASE_SP_DEF = 50
-        self.BASE_SPD = 65
-        self.set_stats()
-
-
-class Squirtle(Pokemon):
-    def __init__(self, level, owner):
-        super().__init__(level)
-        self.set_iv()
-        self.set_ev()
-        self.set_nature()
-        self.owner = owner
-        self.growth_group = "med_slow"
-        self.type = "Water"
-        self.BASE_HP = 44
-        self.BASE_ATK = 48
-        self.BASE_DEF = 65
-        self.BASE_SP_ATK = 50
-        self.BASE_SP_DEF = 64
-        self.BASE_SPD = 43
-        self.set_stats()
