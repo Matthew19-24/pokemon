183b6ce1ef8242e7e8ec5c2d43660d1c9f66eda9
Matthew19-24 <94659375+Matthew19-24@users.noreply.github.com>
2022-01-09 08:37:13 -0500

    Update
    Updated a lot since last time

--- Pokedex.py
+++ Pokedex.py
@@ -1,10 +1,15 @@
 import Stats
 import math
+import Moves
 
 
 class Bulbasaur(Stats.Pokemon):
-    def __init__(self, level, owner):
+    def __init__(self, level, owner, wild):
         super().__init__(level)
+        self.image = 'poke_pic/bulbasaur.png'
+        self.wild = wild
+        self.atk_stat_stage = 0
+        self.def_stat_stage = 0
         self.set_iv()
         self.set_ev()
         self.set_nature()
@@ -13,6 +18,7 @@ class Bulbasaur(Stats.Pokemon):
             (1.2 * (self.lvl ** 3)) - (15 * (self.lvl ** 2)) + (100 * self.lvl) - 140)
         self.growth_group = "med_slow"
         self.type = "Grass"
+        self.BASE_EXP = 64
         self.BASE_HP = 45
         self.BASE_ATK = 49
         self.BASE_DEF = 49
@@ -20,11 +26,23 @@ class Bulbasaur(Stats.Pokemon):
         self.BASE_SP_DEF = 65
         self.BASE_SPD = 45
         self.set_stats()
+        self.attack_1 = Moves.Tackle()
+        self.attack_2 = Moves.Growl()
+        self.attack_3 = None
+        self.attack_4 = None
+
+    @staticmethod
+    def effort(target):
+        target.ev_sp_atk += 1
 
 
 class Charmander(Stats.Pokemon):
-    def __init__(self, level, owner):
+    def __init__(self, level, owner, wild):
         super().__init__(level)
+        self.wild = wild
+        self.image = 'poke_pic/charmander.png'
+        self.atk_stat_stage = 0
+        self.def_stat_stage = 0
         self.set_iv()
         self.set_ev()
         self.set_nature()
@@ -33,6 +51,7 @@ class Charmander(Stats.Pokemon):
             (1.2 * (self.lvl ** 3)) - (15 * (self.lvl ** 2)) + (100 * self.lvl) - 140)
         self.growth_group = "med_slow"
         self.type = "Fire"
+        self.BASE_EXP = 62
         self.BASE_HP = 39
         self.BASE_ATK = 52
         self.BASE_DEF = 43
@@ -40,11 +59,23 @@ class Charmander(Stats.Pokemon):
         self.BASE_SP_DEF = 50
         self.BASE_SPD = 65
         self.set_stats()
+        self.attack_1 = Moves.Scratch()
+        self.attack_2 = Moves.Growl()
+        self.attack_3 = None
+        self.attack_4 = None
+
+    @staticmethod
+    def effort(target):
+        target.ev_spd += 1
 
 
 class Squirtle(Stats.Pokemon):
-    def __init__(self, level, owner):
+    def __init__(self, level, owner, wild):
         super().__init__(level)
+        self.wild = wild
+        self.image = 'poke_pic/squirtle.gif'
+        self.atk_stat_stage = 0
+        self.def_stat_stage = 0
         self.set_iv()
         self.set_ev()
         self.set_nature()
@@ -53,6 +84,7 @@ class Squirtle(Stats.Pokemon):
             (1.2 * (self.lvl ** 3)) - (15 * (self.lvl ** 2)) + (100 * self.lvl) - 140)
         self.growth_group = "med_slow"
         self.type = "Water"
+        self.BASE_EXP = 63
         self.BASE_HP = 44
         self.BASE_ATK = 48
         self.BASE_DEF = 65
@@ -60,3 +92,11 @@ class Squirtle(Stats.Pokemon):
         self.BASE_SP_DEF = 64
         self.BASE_SPD = 43
         self.set_stats()
+        self.attack_1 = Moves.Tackle()
+        self.attack_2 = Moves.Tail_Whip()
+        self.attack_3 = None
+        self.attack_4 = None
+
+    @staticmethod
+    def effort(target):
+        target.ev_def += 1
